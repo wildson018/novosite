47d3a392e4290ed3a777010413f22440ca24dd53
wildson018 <wildson117@gmail.com>
2024-08-02 20:46:52 -0400

    primeiro template

--- novosite/settings.py
+++ novosite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from os
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'usuarios.apps.UsuariosConfig'
+    
 ]
 
 ROOT_URLCONF = 'novosite.urls'
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
 
-# Create your views here.
+def login(request):
+    if request.method =="GET":
+        return render(request, 'usuarios/login.html')
+    else:
+        username = request.POST.get('email') 
+        senha = request.POST.get('senha')
+
+        user = authenticate(username = username, password = senha)
+
+        if user:
+            login_django(request, user)
+            return HttpResponse ('autenticado!')
+        else: 
+            return HttpResponse('E-mail ou senha inválidos!')
+        
+
+
