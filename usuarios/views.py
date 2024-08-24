a1f9532cadab4c14e9cebd2035d0fdbb15b9f35f
wildson018 <wildson117@gmail.com>
2024-08-24 14:14:38 -0400

    aula 23/08

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,38 +4,39 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 
-def login(request):
-    if request.method =="GET":
+
+def login (request):
+    if request.method == "GET":
         return render(request, 'usuarios/login.html')
     else:
-        username = request.POST.get('email') 
-        senha = request.POST.get('senha')
-
-        user = authenticate(username = username, password = senha)
-
+        username = request.POST.get ('email')
+        senha =  request.POST.get ('senha')
+        
+        user = authenticate (username = username, password = senha)
+        
         if user:
             login_django(request, user)
-            return HttpResponse ('autenticado!')
-        else: 
-            return HttpResponse('E-mail ou senha inválidos!')
-        
-def cadastro(request):
-    if request.method=="GET":
-        return render(request, 'usuarios/cadastro.html')
-    else:
-        username= request.POST.get('email')
-        email= request.POST.get('email')
-        password= request.POST.get('senha')
-        first_name= request.POST.get('nome')
-
-        user = User.objects.filter(username=username).first()
-        
-        if user:
-            return HttpResponse("Usuario já existente!")
+            return HttpResponse('Autenticado')
         else:
-            user = User.objects.create_user(username=username,email=email,password=password,first_name=first_name)
-            user.save()
+            return HttpResponse('E-mail ou senha invalidos!')
 
-            return HttpResponse("usuario cadastro com sucesso!")
+def cadastro(request):
+  if request.method == "GET":
+    return render(request, 'usuarios/cadastro.html')
+  else:
+    username = request.POST.get('email')
+    email = request.POST.get('email')
+    password = request.POST.get('senha')
+    first_name = request.POST.get('firstname')
+
+    user =  User.objects.filter(username=username).first()
+
+    if user:
+      return HttpResponse("Usuário já existente!")
+    else:
+      user = User.objects.create_user(username=username,email=email,password=password,first_name=first_name)
+      user.save()
+
+      return HttpResponse("Usuário cadastro com sucesso!")
 
 
